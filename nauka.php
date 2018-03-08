4a48645873308bc1ddd2001ed3f80ff34a6f3fd4
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-08 13:05:12 +0100

    0.1
    test

---
+++ nauka.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>nauka php</title>
+    </head>
+    <body>
+        <h1> 
+            <?php
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            ?>
+        </h1>
+    </body>
+
+</html>
