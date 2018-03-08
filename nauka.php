b51af2768a0145e0242496d2af364a03ebf15d9e
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-08 22:16:01 +0100

    testy 0.1

--- nauka.php
+++ nauka.php
@@ -1,24 +1,15 @@
-<html>
-    <head>
-        <title>nauka php</title>
-    </head>
-    <body>
-        <h1> 
-            <?php
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            ?>
-        </h1>
-    </body>
 
-</html>
+<?php
+
+    echo "s"; //komentarz do linijki /n
+
+    echo "tekst tekst";  /*linijka komentarza
+            dalej linijka komentarza
+            tu sie konczy */
+    $nazwa1 = 1; //do zmiennej "nazwa1" jest przypisana 1
+    $nazwa2 = 2; //do zmiennej "nazwa2" jest przypisana 2
+    echo "1+0=$nazwa1";
+    echo "1+1=$nazwa2";
+    echo '$nazwa1';
+    echo $nazwa2;
+?>
