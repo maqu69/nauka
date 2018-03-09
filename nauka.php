94104adbdbbbb28d9b17b05ec4259a31ce81e0a4
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-09 16:46:18 +0100

    testy 0.1

--- nauka.php
+++ nauka.php
@@ -1,7 +1,7 @@
 
 <?php
 
-    echo "siema<br>"; //komentarz do linijki
+    echo "siema<br>"; #komentarz do linijki
 
     echo "tekst tekst<br>";  /*linijka komentarza
             dalej linijka komentarza
@@ -12,4 +12,17 @@
     echo "1+1=$nazwa2<br>";
     echo '$nazwa1<br>';
     echo "$nazwa2<br>";
-?>
+
+
+    echo "------------- Typy zmiennych----------------<br>";
+        $a = 10.5; #liczy dziesietne
+
+    echo "Hania ma $a lat<br>";
+        settype($a, "integer");
+    echo "Hania ma $a lat"; #po zmianie zmiennej przez settype
+
+
+
+
+    ?>
+
