aa6618d27f3212212585beff97022e0f25bc9cb1
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-12 14:50:41 +0100

    testy 0.7

--- whiledo.php
+++ whiledo.php
@@ -2,16 +2,19 @@
 
 
 
-    $x=1;
+    $x=11;
 
     while ($x<=10){
     $x++;
     echo "x = $x<br>";
 }
 
-
-
-
+    $a = 10;
+    do {
+        echo" $a<br>";
+        $a++;
+    }
+    while ($a<101)  //warunek kontynuowania petli
 
 
 
