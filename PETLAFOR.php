d4685a34a7d1a9a1598461efd227f6daf69aab32
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-12 14:04:50 +0100

    testy 0.5

---
+++ PETLAFOR.php
@@ -0,0 +1,44 @@
+<?php
+
+
+  /*for( inicjalizacja zmiennych ; sprawdzenie warunku ; modyfikacja zmiennych) {
+
+    blok wyrażeń
+
+}
+*/
+
+  for ($x=1; $x<=10; $x++)
+        echo "x = $x<br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ?>
