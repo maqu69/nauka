3e25d4b306ebdee0121d00b60ff765cd6d5d5149
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-14 12:11:46 +0100

    testy 0.8

--- ciagitablic.php
+++ ciagitablic.php
@@ -4,10 +4,30 @@ echo "PHP umożliwia zamianę ciągów na tablice i odwrotnie. Zamiana ciągu na
 $dane = "PHP umożliwia zamianę ciągów na tablice i odwrotnie. Zamiana ciągu na tablicę jest bardzo przydatna jeśli zachodzi potrzeba wyciągnięcie jakiegoś fragmentu danych z ciągu";
 echo "<br>";
 
-$tablica = explode (" ", $dane); //umozliwia nam wyciagniecie danych z konkretnego pliku/tekstu/kodu
+$tablica = explode (" ", $dane); //zamienia ciag na tablice
 
 echo $tablica[1]."<br>";
 echo $tablica[5]."<br>";
 
-echo count($tablica). ", tyle nzajduje sie spacji w tekscie"; //policzylismy ile jest spacji w tekscie $dame
+echo count($tablica). ", tyle znajduje sie spacji w tekscie<br>"; //policzylismy ile jest spacji w tekscie $dame
+
+
+
+$uzytkownik = "Piotr,Nowak,Warszawa,Informatyk";
+$tablica = explode(",", $uzytkownik);
+
+echo "$tablica[0] <br>";
+echo "$tablica[1] <br>";
+echo "$tablica[2] <br>";
+echo "$tablica[3] <br>";
+
+
+echo "-----funkcja implode-----<br>";
+
+$tab = array("Polska","Niemcy","Francja");
+$ciag = implode(", ",$tab); // zamienia tablice na ciag
+
+echo $ciag;
+
+
 ?>
