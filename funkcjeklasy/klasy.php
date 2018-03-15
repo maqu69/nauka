19ede1a9002bc7c611512e42c7be85552253c26d
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-15 18:49:31 +0100

    testy 0.8

--- funkcjeklasy/klasy.php
+++ funkcjeklasy/klasy.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Kuba
- * Date: 15.03.2018
- * Time: 14:40
- */
+// pcozatek klasy koszyk
+
+class koszyk {
+    // Definicja zmiennej - tablicy asocjacyjnej zawierającej numery artykułów i
+    // ich ilość
+var $artykuly;
+
+function dodaj($numer, $ilosc){
+
+    $this->artykuly["$numer"] += $ilosc; //metoda dodajaca podana ilosc artykulow do koszyka
+}
+function usun($numer, $ilosc){ //metoda usuwajaca podana ilosc artykulwo  ksozyka
+    if($this->artykuly["$numer"]>$ilosc)
+        $this->artykuly["$numer"]-=$ilosc;
+    else
+        $this->artykuly["$numer"]=0;
+}
+//metoda wyswietlajaca zawartosc koszyka
+
+function wyswietl(){
+    while(list($k, $v) = each($this->artykuly))
+        if($v>0)
+            echo "Artykul nr $k - $v sztuk<br>";
+}
+
+}
+
+//przyklad wykorzystania
+
+$koszyk = new Koszyk;
+$koszyk->dodaj('20', 2);
+$koszyk->dodaj('12', 4);
+$koszyk->dodaj('20', 5);
+$koszyk->usun('12', 4);
+$koszyk->wyswietl();
+?>
---
+++ funkcjeklasy/klasy1.php
@@ -0,0 +1,25 @@
+<?php
+class jablko {
+    var $kolor = "czerwony"; // wartosci nei musimy dopisywac, jesli podajemy w funkcji zmienne $jakie, $ilewazy
+    var $waga = 5; // wartosci nei musimy dopisywac, jesli podajemy w funkcji zmienne $jakie, $ilewazy
+
+    function jablko($jakie, $ilewazy){
+        $this->kolor = $jakie;
+        $this->waga = $ilewazy;
+
+
+    }
+    function ugryz(){
+        $this->waga--;
+    }
+}
+$ojablko = new jablko('czerwone', '10' );
+echo ($ojablko->kolor." ".$ojablko->waga.'<br>');
+
+$ojablko->ugryz();
+echo ($ojablko->kolor." ".$ojablko->waga.'<br>');
+
+
+
+
+?>
