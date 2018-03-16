bcf16448198e18e22b077444b138399718856a3c
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-16 20:46:40 +0100

    testy

--- cwiczenia/cwiczenie1.php
+++ cwiczenia/cwiczenie1.php
@@ -1,4 +1,66 @@
 <?php
 
+function rentcost($days){
+    echo "wynajem trwa 1 dzien, koszt pobytu 200zl";
+}
 
-function rentCost($days, )
+
+echo rentcost();
+
+
+
+
+function wypisz($a){
+    echo 'argument z listy argumentow $a = '.$a;
+    echo 'ponadto mamy '.(func_num_args()-1).'
+    dodatkowych argumentów oto one :';
+    for($i=1; $i < func_num_args(); ++$i){
+        echo func_get_arg($i);
+    }
+}
+wypisz(1,2,3,4,5,6);
+
+
+
+
+function rentCost($days){
+    if ($days=1; $days
+    echo "wynajecie pokoju kosztuje "
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
