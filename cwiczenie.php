342e395b52dcd5d7fb86940676a82007bfba2680
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-16 18:22:23 +0100

    testy 0.8

---
+++ cwiczenie.php
@@ -0,0 +1,27 @@
+<html>
+<head></body>
+<body>
+Podaj liczbe sekund<br />
+<form action="sekundy.php" method="post">
+    <input type="text" name="sekundy"><br />
+    <input type="submit" method="post" value="Wyslij" />
+</form>
+
+<?php
+$sekundy = $_POST['sekundy'];
+echo "Ilosc podanych sekund wynosi: ".$sekundy."<BR /><BR />";
+
+$minuty=$sekundy/60;
+$godziny=$sekundy/3600;
+$dni=$godziny/24;
+$lata=$dni/365;
+
+echo "Ilosc minut wynosi: ".$minuty."<BR />";
+echo "Ilosc godzin wynosi: ".$godziny."<BR />";
+echo "Ilosc dni wynosi: ".$dni."<BR />";
+echo "Ilosc lat wynosi: ".$lata."<BR />";
+
+?>
+
+</body>
+</html>
