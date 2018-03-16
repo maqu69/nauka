329b122345b018eb4e55e40249f4c9e4d356c93c
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-16 17:58:28 +0100

    testy 0.8

---
+++ formularz.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Kurs php - podstawy</title>
+    <meta http-equiv="content-type" content="text/html"; charset="utf-8" />
+    <meta name="description" content="kurs php">
+    <meta name="keywords" content="html, php, kurs">
+</head>
+<body>
+    <form action="formularz.php" method="POST">
+        <p>Podaj imie;</p>
+        <input type="text" name="imie"><br>
+        <input type="submit" name="button" value="wyslij">
+
+    </form>
+
+</body>
+</html>
