a48bb7979dafa885d27d7f4e7d963469fa773c77
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-23 11:55:00 +0100

    l;k

--- zadania1-5/zadanie4.php
+++ zadania1-5/zadanie4.php
@@ -1,19 +1,21 @@
 <?php
 
-$n1=1;
-$n2=4;
-
 
 
 function max2($n1, $n2) {
-    if($n1 > $n2)
-        echo $n1;
-        return;
-    if($n2 > $n1)
-        echo $n2;
-        return;
+    if($n1 > $n2) {
+        return $n1;
+    }
+    else {
+        return $n2;
+    }
 
 }
+echo max2(15,300);
+
+//https://3v4l.org/SPJOl rozwiazanie nr1
+//https://3v4l.org/umenM rozwiazanie nr2
+
 
 
 
