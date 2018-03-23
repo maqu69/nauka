2ff993145a7738545ad0aa567199317a5f24e2ab
jakub lenart <37178022+maqu69@users.noreply.github.com>
2018-03-23 11:16:57 +0100

    testy

---
+++ zadania1-5/zadanie4.php
@@ -0,0 +1,20 @@
+<?php
+
+$n1=1;
+$n2=4;
+
+
+
+function max2($n1, $n2) {
+    if($n1 > $n2)
+        echo $n1;
+        return;
+    if($n2 > $n1)
+        echo $n2;
+        return;
+
+}
+
+
+
+?>
